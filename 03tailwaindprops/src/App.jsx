a8a86d872ac40efbe77e341db04bd7734f635583
Vedant Hippalgave <158559140+vedanth2174@users.noreply.github.com>
2025-01-29 14:00:10 +0000

    finally after very much time c*utya error of not showing cards was solved

--- 03tailwaindprops/src/App.jsx
+++ 03tailwaindprops/src/App.jsx
@@ -1,8 +1,10 @@
-
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,7 @@ function App() {
   return (
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-14'>Tailwind Test</h1>
+    <Card />
     </>
   )
 }
