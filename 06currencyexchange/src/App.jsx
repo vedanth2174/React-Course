99ccba3b7f97baa896292239ae5a8efaaa410857
Vedant Hippalgave <158559140+vedanth2174@users.noreply.github.com>
2025-02-02 13:36:47 +0000

    started with currency convertor and designned custom hook.

---
+++ 06currencyexchange/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import InputBox from './InputBox';
+
+function App() {
+
+  return (
+    <>
+    <h1 className='heading'>Currency Convertor</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 06currencyexchange/src/hooks/useCurrency.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+//https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json
+function useCurrency(currency){
+    const[data, setData] = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res)=>setData(res[currency]))
+        console.log(data)
+    }, [currency])//want to call this method whenever currency changes
+    console.log(data)
+    return data
+}
+
+export default useCurrency;
